d946c7082de3c874cfa7ff8658a1e07107a7ecd9
BlueSlime <jeremylevilain78@orange.fr>
2015-07-18 00:10:25 +0200

    SamaGamesAPI Update

--- src/main/java/net/samagames/core/api/games/GameProperties.java
+++ src/main/java/net/samagames/core/api/games/GameProperties.java
@@ -1,6 +1,5 @@
 package net.samagames.core.api.games;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import net.samagames.api.games.IGameProperties;
@@ -10,27 +9,22 @@ import org.bukkit.Bukkit;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.HashMap;
 import java.util.logging.Level;
 
 public class GameProperties implements IGameProperties
 {
-    private final HashMap<String, String> options;
-
     private String mapName;
+    private JsonObject options;
     private int minSlots;
     private int maxSlots;
 
     public GameProperties()
     {
-        this.options = new HashMap<>();
         this.reload();
     }
 
     public void reload()
     {
-        this.options.clear();
-
         try
         {
             File file = new File(APIPlugin.getInstance().getDataFolder().getAbsoluteFile().getParentFile().getParentFile(), "game.json");
@@ -43,14 +37,7 @@ public class GameProperties implements IGameProperties
             this.mapName = rootJson.get("map-name").getAsString();
             this.minSlots = rootJson.get("min-slots").getAsInt();
             this.maxSlots = rootJson.get("max-slots").getAsInt();
-
-            JsonArray optionsJson = rootJson.get("options").getAsJsonArray();
-
-            for(int i = 0; i < optionsJson.size(); i++)
-            {
-                JsonObject optionJson = optionsJson.get(i).getAsJsonObject();
-                this.options.put(optionJson.get("key").getAsString(), optionJson.get("value").getAsString());
-            }
+            this.options = rootJson.get("options").getAsJsonObject();
         }
         catch (FileNotFoundException e)
         {
@@ -76,15 +63,7 @@ public class GameProperties implements IGameProperties
         return this.maxSlots;
     }
 
-    public String getOption(String key)
-    {
-        if(this.options.containsKey(key))
-            return this.options.get(key);
-        else
-            return null;
-    }
-
-    public HashMap<String, String> getOptions()
+    public JsonObject getOptions()
     {
         return this.options;
     }
