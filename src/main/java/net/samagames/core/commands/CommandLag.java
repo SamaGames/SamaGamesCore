4d811c8bc37e215de1d0fd6050033f988d2c8f3a
BlueSlime <jeremylevilain78@orange.fr>
2015-09-28 23:54:27 +0200

    New friendly /lag command

--- src/main/java/net/samagames/core/commands/CommandLag.java
+++ src/main/java/net/samagames/core/commands/CommandLag.java
@@ -1,13 +1,16 @@
 package net.samagames.core.commands;
 
-import net.md_5.bungee.api.ChatColor;
 import net.minecraft.server.v1_8_R3.MinecraftServer;
 import net.samagames.core.APIPlugin;
+import net.samagames.tools.chat.ChatUtils;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * This file is a part of the SamaGames project
@@ -16,17 +19,22 @@ import java.util.Date;
  * (C) Copyright Elydra Network 2015
  * All rights reserved.
  */
-public class CommandLag extends AbstractCommand {
-	public CommandLag(APIPlugin plugin) {
+public class CommandLag extends AbstractCommand
+{
+    private final SimpleDateFormat dateFormat;
+
+	public CommandLag(APIPlugin plugin)
+    {
 		super(plugin);
+
+        this.dateFormat = new SimpleDateFormat("dd.MM.yyyy à HH:mm:ss", Locale.FRANCE);
 	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, String label, String[] arguments) {
-		if (!(sender instanceof Player)) {
-			sender.sendMessage(ChatColor.GOLD + "Latence : 0.0 ms");
+	public boolean onCommand(CommandSender sender, String label, String[] arguments)
+    {
+		if (!(sender instanceof Player))
 			return true;
-		}
 
 		Player player = (Player) sender;
 		int latency = ((CraftPlayer) player).getHandle().ping;
@@ -35,32 +43,34 @@ public class CommandLag extends AbstractCommand {
 		double[] tab;
 		int length = (tab = MinecraftServer.getServer().recentTps).length;
 
-		for(int var7 = 0; var7 < length; ++var7) {
+		for(int var7 = 0; var7 < length; ++var7)
+        {
 			tps.append(this.format(tab[var7]));
+
 			if (var7 + 1 < length)
 				tps.append(", ");
 		}
 
-		String server = APIPlugin.getInstance().getServerName();
-
-		player.sendMessage(org.bukkit.ChatColor.YELLOW + "------[ Informations de Lag ]------");
-		player.sendMessage(ChatColor.GOLD + " ");
-		player.sendMessage(ChatColor.YELLOW + "Serveur : " + ChatColor.AQUA + server + " " + ChatColor.YELLOW + new Date());
-		player.sendMessage(ChatColor.GOLD + " ");
-		player.sendMessage(ChatColor.YELLOW + "Latence avec le server (ping) : " + ChatColor.AQUA + formatLag(latency) + " ms");
-		player.sendMessage(ChatColor.GOLD + " ");
-		player.sendMessage(ChatColor.YELLOW + "Ticks par seconde (" + ChatColor.GREEN + "20 = " + ChatColor.AQUA + "Parfait" + ChatColor.YELLOW + ")");
-		player.sendMessage(tps.toString());
+		player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+		player.sendMessage(ChatUtils.getCenteredText(ChatColor.WHITE + "•" + ChatColor.BOLD + " Informations de lag " + ChatColor.RESET + ChatColor.WHITE + "•"));
+        player.sendMessage(ChatColor.YELLOW + "Date : " + ChatColor.GRAY + this.dateFormat.format(new Date()));
+        player.sendMessage(ChatColor.YELLOW + "Serveur : " + ChatColor.GRAY + APIPlugin.getInstance().getServerName());
+		player.sendMessage("");
+		player.sendMessage(ChatColor.YELLOW + "Latence : " + this.formatLag(latency) + ChatColor.GRAY + " ms");
+		player.sendMessage(ChatColor.YELLOW + "Charge serveur : " + ChatColor.GRAY + tps.toString());
+        player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
 
 		return true;
 	}
 
 
-	private String format(double tps) {
+	private String format(double tps)
+    {
 		return (tps > 18.0D? org.bukkit.ChatColor.GREEN:(tps > 16.0D? org.bukkit.ChatColor.YELLOW: org.bukkit.ChatColor.RED)).toString() + (tps > 20.0D?"*":"") + Math.min((double)Math.round(tps * 100.0D) / 100.0D, 20.0D);
 	}
 
-	private String formatLag(double lag) {
+	private String formatLag(double lag)
+    {
 		return "" + (lag > 200.0 ? org.bukkit.ChatColor.RED : (lag > 120D? org.bukkit.ChatColor.GOLD: (lag > 70D? org.bukkit.ChatColor.YELLOW: org.bukkit.ChatColor.GREEN))).toString() + (double)Math.round(lag * 100.0D) / 100.0D;
 	}
 }
