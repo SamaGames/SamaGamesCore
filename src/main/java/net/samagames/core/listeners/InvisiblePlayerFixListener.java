869b909b3149d994486011c53cc26b23263ffc47
Silvanosky <geekpower14@gmail.com>
2015-10-13 14:30:39 +0200

    Fix player not connected

--- src/main/java/net/samagames/core/listeners/InvisiblePlayerFixListener.java
+++ src/main/java/net/samagames/core/listeners/InvisiblePlayerFixListener.java
@@ -9,13 +9,17 @@ import net.samagames.restfull.RestAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 
 import java.util.UUID;
 
 /**
  * Created by Silva on 13/10/2015.
  */
-public class InvisiblePlayerFixListener implements IJoinHandler
+public class InvisiblePlayerFixListener implements Listener
 {
     private final APIPlugin pluginAPI;
 
@@ -24,42 +28,37 @@ public class InvisiblePlayerFixListener implements IJoinHandler
         this.pluginAPI = pluginAPI;
     }
 
-    @Override
-    public void onLogin(UUID player, String username)
+    @EventHandler
+    public void onPlayerJoin(PlayerLoginEvent event)
     {
         try{
-            sendPlayerToAll(player);
-            sendAllToPlayer(player);
+            sendPlayerToAll(event.getPlayer());
+            sendAllToPlayer(event.getPlayer());
         }catch (Exception e)
         {
             e.printStackTrace();
         }
-
     }
 
-    public void sendPlayerToAll(UUID player)
+    public void sendAllToPlayer(Player current)
     {
         Bukkit.getScheduler().runTaskAsynchronously(pluginAPI, () -> {
-            EntityPlayer entity = ((CraftPlayer)Bukkit.getPlayer(player)).getHandle();
-            PacketPlayOutPlayerInfo packet = new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, entity);
 
-            for(Player p : Bukkit.getOnlinePlayers())
-            {
-                ((CraftPlayer)p).getHandle().playerConnection.sendPacket(packet);
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                EntityPlayer entity = ((CraftPlayer) player).getHandle();
+                ((CraftPlayer) current).getHandle().playerConnection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, entity));
             }
         });
     }
 
-    public void sendAllToPlayer(UUID uuid)
+    public void sendPlayerToAll(Player player)
     {
-        Player p = Bukkit.getPlayer(uuid);
-
         Bukkit.getScheduler().runTaskAsynchronously(pluginAPI, () -> {
+            EntityPlayer entity = ((CraftPlayer) player).getHandle();
+            PacketPlayOutPlayerInfo packet = new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, entity);
 
-            for(Player player : Bukkit.getOnlinePlayers())
-            {
-                EntityPlayer entity = ((CraftPlayer)player).getHandle();
-                ((CraftPlayer)p).getHandle().playerConnection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, entity));
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                ((CraftPlayer) p).getHandle().playerConnection.sendPacket(packet);
             }
         });
     }
