22bb9b96e879f4b256e29d9263ba87c9ce17eb1a
Silvanosky <geekpower14@gmail.com>
2016-03-22 19:57:52 +0100

    Rebuild resource pack implementation

--- src/main/java/net/samagames/core/api/resourcepacks/ResourcePacksManagerImpl.java
+++ src/main/java/net/samagames/core/api/resourcepacks/ResourcePacksManagerImpl.java
@@ -1,28 +1,26 @@
 package net.samagames.core.api.resourcepacks;
 
-import net.minecraft.server.v1_8_R3.PacketPlayInResourcePackStatus;
-import net.minecraft.server.v1_8_R3.PacketPlayOutResourcePackSend;
 import net.samagames.api.SamaGamesAPI;
 import net.samagames.api.resourcepacks.IResourceCallback;
 import net.samagames.api.resourcepacks.IResourcePacksManager;
 import net.samagames.core.APIPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerResourcePackStatusEvent;
 import redis.clients.jedis.Jedis;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.UUID;
 
 /**
  * This file is a part of the SamaGames project
  * This code is absolutely confidential.
+ * Created by Silvanosky
  * (C) Copyright Elydra Network 2015
  * All rights reserved.
  */
@@ -30,19 +28,17 @@ public class ResourcePacksManagerImpl implements IResourcePacksManager, Listener
 {
 
     private final HashSet<UUID> currentlyDownloading = new HashSet<>();
-    private final HashMap<UUID, KillerTask> killers = new HashMap<>();
     private final SamaGamesAPI api;
     private final String resetUrl;
-    private final ProtocolHandler handler;
     private String forceUrl;
-    private String forceHash;
     private IResourceCallback callback;
+
+    private final static String rejectMessage = ChatColor.RED + "Il est nécessaire d'accepter le ressource pack pour jouer.";
 
     public ResourcePacksManagerImpl(SamaGamesAPI api)
     {
         Bukkit.getPluginManager().registerEvents(this, APIPlugin.getInstance());
 
-        this.handler = new ProtocolHandler(APIPlugin.getInstance(), this);
         this.api = api;
 
         Jedis jedis = api.getBungeeResource();
@@ -61,51 +57,25 @@ public class ResourcePacksManagerImpl implements IResourcePacksManager, Listener
     public void forcePack(String name, IResourceCallback callback)
     {
         Jedis jedis = api.getBungeeResource();
-        forcePack(jedis.hget("resourcepack:" + name, "url"), jedis.hget("resourcepack:" + name, "hash"), callback);
+        forceUrlPack(jedis.hget("resourcepack:" + name, "url"), callback);
         jedis.close();
     }
 
     @Override
-    public void forcePack(String url, String hash, IResourceCallback callback)
+    public void forceUrlPack(String url, IResourceCallback callback)
     {
         Bukkit.getScheduler().runTaskAsynchronously(APIPlugin.getInstance(), () -> {
             forceUrl = url;
-            forceHash = hash;
-            APIPlugin.getInstance().getLogger().info("Defined automatic resource pack : " + forceUrl + " with hash " + forceHash);
+            APIPlugin.getInstance().getLogger().info("Defined automatic resource pack : " + forceUrl);
         });
 
         this.callback = callback;
     }
 
-    private void sendPack(Player player, String url, String hash)
+    private void sendPack(Player player, String url)
     {
-        APIPlugin.getInstance().getLogger().info("Sending pack to " + player.getName() + " : " + url + " with hash " + hash);
-
-        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(new PacketPlayOutResourcePackSend(url, hash));
-    }
-
-    void handle(Player player, String hash, PacketPlayInResourcePackStatus.EnumResourcePackStatus state)
-    {
-        if (forceUrl == null || forceHash == null)
-            return;
-
-        if (callback != null)
-            callback.callback(player, state);
-
-        if (state == PacketPlayInResourcePackStatus.EnumResourcePackStatus.SUCCESSFULLY_LOADED)
-        {
-            currentlyDownloading.remove(player.getUniqueId());
-            Bukkit.getScheduler().runTaskAsynchronously(APIPlugin.getInstance(), () -> {
-                Jedis jedis = api.getBungeeResource();
-                jedis.sadd("playersWithPack", player.getUniqueId().toString());
-                jedis.close();
-            });
-        }
-
-        if (killers.get(player.getUniqueId()) != null)
-        {
-            killers.get(player.getUniqueId()).changeState(state);
-        }
+        player.setResourcePack(url);
+        APIPlugin.getInstance().getLogger().info("Sending pack to " + player.getName() + " : " + url);
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
@@ -113,15 +83,18 @@ public class ResourcePacksManagerImpl implements IResourcePacksManager, Listener
     {
         final Player player = event.getPlayer();
 
-        if (forceHash != null && forceUrl != null)
+        if (forceUrl != null)
         {
             Bukkit.getScheduler().runTaskLater(APIPlugin.getInstance(), () -> {
                 currentlyDownloading.add(player.getUniqueId());
-                sendPack(player, forceUrl, forceHash);
+                sendPack(player, forceUrl);
 
-                KillerTask task = new KillerTask(player, callback, this);
-                task.runTaskTimer(APIPlugin.getInstance(), 20L, 20L);
-                killers.put(event.getPlayer().getUniqueId(), task);
+                //Kick if still downloading after 5 minutes
+                Bukkit.getScheduler().runTaskLater(SamaGamesAPI.get().getPlugin(),
+                        () -> {
+                            if(currentlyDownloading.contains(player.getUniqueId()))
+                                player.kickPlayer(rejectMessage);
+                        }, 2400L);
             }, 100);
         } else
         {
@@ -131,10 +104,41 @@ public class ResourcePacksManagerImpl implements IResourcePacksManager, Listener
                 jedis.close();
 
                 if (l > 0)
-                    ((CraftPlayer) player).getHandle().playerConnection.sendPacket(new PacketPlayOutResourcePackSend(resetUrl, "samareset.zip"));
+                    player.setResourcePack(resetUrl);
             }, 100);
         }
     }
+
+    @EventHandler
+    public void onResourcePack(PlayerResourcePackStatusEvent event)
+    {
+        Player player = event.getPlayer();
+
+        if (forceUrl == null)
+            return;
+
+        if (callback != null)
+            callback.callback(player, event.getStatus());
+
+        if (event.getStatus().equals(PlayerResourcePackStatusEvent.Status.DECLINED)
+                || event.getStatus().equals(PlayerResourcePackStatusEvent.Status.FAILED_DOWNLOAD))
+        {
+            if (callback == null || callback.automaticKick(player))
+            {
+                Bukkit.getScheduler().runTask(SamaGamesAPI.get().getPlugin(), () -> player.kickPlayer(rejectMessage));
+            }
+            currentlyDownloading.remove(player.getUniqueId());
+        }else if(event.getStatus().equals(PlayerResourcePackStatusEvent.Status.SUCCESSFULLY_LOADED))
+        {
+            currentlyDownloading.remove(player.getUniqueId());
+            Bukkit.getScheduler().runTaskAsynchronously(APIPlugin.getInstance(), () -> {
+                Jedis jedis = api.getBungeeResource();
+                jedis.sadd("playersWithPack", player.getUniqueId().toString());
+                jedis.close();
+            });
+        }
+    }
+
 
     @Override
     public void kickAllUndownloaded()
@@ -143,19 +147,9 @@ public class ResourcePacksManagerImpl implements IResourcePacksManager, Listener
         {
             Player player = Bukkit.getPlayer(id);
             if (player != null)
-                player.kickPlayer(ChatColor.RED + "Il est nécessaire d'accepter le ressource pack pour jouer.");
+                player.kickPlayer(rejectMessage);
         }
 
         currentlyDownloading.clear();
-        killers.values().forEach(net.samagames.core.api.resourcepacks.KillerTask::cancel);
-        killers.clear();
-    }
-
-    void removeKillerFor(UUID player) {
-        if (killers.containsKey(player))
-        {
-            killers.get(player).cancel();
-            killers.remove(player);
-        }
     }
 }
