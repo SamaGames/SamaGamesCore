f67d87a6a130c42258abfca9a5382618abf8d71b
BlueSlime <jeremylevilain78@orange.fr>
2016-05-07 17:33:51 +0200

    refresh() must call refresh(force = true)

--- src/main/java/net/samagames/core/api/shops/PlayerShop.java
+++ src/main/java/net/samagames/core/api/shops/PlayerShop.java
@@ -45,7 +45,7 @@ public class PlayerShop implements IPlayerShop {
     @Override
     public void refresh()
     {
-        refresh(false);
+        refresh(true);
     }
 
     public void refresh(boolean force)
