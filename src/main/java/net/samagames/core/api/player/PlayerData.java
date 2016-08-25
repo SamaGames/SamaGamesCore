1573413e2936ad24a139c1a6902201830bd97c87
Silvanosky <geekpower14@gmail.com>
2016-08-25 16:14:55 +0200

    fix compile

--- src/main/java/net/samagames/core/api/player/PlayerData.java
+++ src/main/java/net/samagames/core/api/player/PlayerData.java
@@ -61,7 +61,6 @@ public class PlayerData extends AbstractPlayerData
                 0,
                 null,
                 null,
-                0,
                 null,
                 null,
                 0);
