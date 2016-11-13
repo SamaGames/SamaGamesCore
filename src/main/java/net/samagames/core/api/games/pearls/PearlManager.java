06c33c166518a470dfc0f8ec6aecd3d9419a15bf
BlueSlime <jeremylevilain78@orange.fr>
2016-11-13 14:49:17 +0100

    Applying the new api

--- src/main/java/net/samagames/core/api/games/pearls/PearlManager.java
+++ src/main/java/net/samagames/core/api/games/pearls/PearlManager.java
@@ -88,10 +88,8 @@ public class PearlManager implements IPearlManager
     }
 
     @Override
-    public void runGiveAlgorythm(Player player, int gameTime, boolean win)
+    public Pearl runGiveAlgorythm(Player player, int gameTime, boolean win)
     {
-        Bukkit.broadcastMessage("Running algorithm for the player: " + player.getName());
-
         int playerRankId = (int) this.api.getPlayerManager().getPlayerData(player.getUniqueId()).getPlayerBean().getGroupId();
 
         double rankMultiplier = playerRankId > 1 ? Double.parseDouble("1." + (playerRankId < 6 ? 5 : playerRankId - 1)) : 1.0D;
@@ -101,8 +99,6 @@ public class PearlManager implements IPearlManager
 
         int pearlChance = (int) ((gameTime / 2) * rankMultiplier * (win ? 1.2 : 0.8));
         int random = new Random().nextInt(100);
-
-        Bukkit.broadcastMessage("Player " + player.getName() + ": " + pearlChance + "% -> " + random);
 
         if (random <= pearlChance)
         {
@@ -118,11 +114,9 @@ public class PearlManager implements IPearlManager
             jedis.expire("pearls:" + player.getUniqueId().toString() + ":" + pearl.getUUID().toString(), (int) TimeUnit.MILLISECONDS.toSeconds(pearl.getExpiration()));
             jedis.close();
 
-            player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
-            player.sendMessage(ChatUtils.getCenteredText(ChatColor.GREEN + "\u25C9"));
-            player.sendMessage(ChatUtils.getCenteredText(ChatColor.WHITE + "Vous avez trouvé une " + ChatColor.GREEN + "perle de niveau " + stars + ChatColor.WHITE + " !"));
-            player.sendMessage(ChatUtils.getCenteredText(ChatColor.WHITE + "Echangez-la auprès de " + ChatColor.GOLD + "Graou" + ChatColor.WHITE + " dans le Hub !"));
-            player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+            return pearl;
         }
+
+        return null;
     }
 }
